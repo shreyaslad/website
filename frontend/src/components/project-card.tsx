6a867e19321b12e60f8d595a6e36c44824395b95
Shreyas <slad0716@gmail.com>
2023-10-24 00:10:38 -0700

    fix: update links for buttons

--- frontend/src/components/project-card.tsx
+++ frontend/src/components/project-card.tsx
@@ -14,28 +14,31 @@ type CardProps = {
   title: string;
   description: string;
   tags: string[];
+  link: string;
 };
 
 export function ProjectCard(props: CardProps) {
-  const { title, description, tags } = props;
+  const { title, description, tags, link } = props;
 
   return (
-    <Card className="hover:bg-muted" key={title}>
-      <CardHeader>
-        <div className="flex flex-row justify-between items-center">
-          <CardTitle>{title}</CardTitle>
-          <ExternalLink className="w-4 h-4" />
-        </div>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
+    <a href={link} target="_blank">
+      <Card className="hover:bg-muted" key={title}>
+        <CardHeader>
+          <div className="flex flex-row justify-between items-center">
+            <CardTitle>{title}</CardTitle>
+            <ExternalLink className="w-4 h-4" />
+          </div>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
 
-      <CardFooter className="gap-2">
-        {tags.map((val, i) => (
-          <Badge variant={"secondary"} key={i}>
-            {val}
-          </Badge>
-        ))}
-      </CardFooter>
-    </Card>
+        <CardFooter className="gap-2">
+          {tags.map((val, i) => (
+            <Badge variant={"secondary"} key={i}>
+              {val}
+            </Badge>
+          ))}
+        </CardFooter>
+      </Card>
+    </a>
   );
 }
